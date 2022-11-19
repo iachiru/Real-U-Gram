8a02afed53bbf81ac38b29b633ed0c0fd3b0ecd9
iachiru <bistancu@gmail.com>
2022-11-19 12:48:10 +0000

    added buttons for login and sign up and greeting message

--- real-u-gram/src/App.js
+++ real-u-gram/src/App.js
@@ -1,11 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 import { useAuth } from "./context/authContext";
 
 function App() {
-  const { user } = useAuth();
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
 
-  if (user) return <div>{user.email}</div>;
-  return <div className="App">Hello World</div>;
+  if (user)
+    return (
+      <div>
+        <h2>Hello {user.email}</h2>
+        <button onClick={logOut}>Log Out</button>
+      </div>
+    );
+  return (
+    <div className="App">
+      <h1>Welcome to Real-U-Gram!</h1>
+      <button onClick={() => navigate("/login")}>Log In</button>
+      <button onClick={() => navigate("/signup")}>Sign Up</button>
+    </div>
+  );
 }
 
 export default App;
