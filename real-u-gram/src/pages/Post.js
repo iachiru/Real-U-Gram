dee0c2e009bd7e4769819378f6f579adcbd8ef72
iachiru <bistancu@gmail.com>
2022-11-27 19:11:57 +0000

    feat: added Post comments, input to add comment. Not connected to database only dummy info for now

--- real-u-gram/src/pages/Post.js
+++ real-u-gram/src/pages/Post.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Post.css";
+import Bookmark from "../images/Bookmark.png";
+import Comment from "../images/Comment.png";
+import Like from "../images/Like.png";
+import Share from "../images/Share.png";
 
 export default function Header({ username, profilePic, postPhoto, caption }) {
+  const comments = [
+    {
+      username: "Mocolin",
+      comment: "Hello from the comments",
+    },
+    { username: "Trolo Lolo", comment: "Bye from the comments" },
+  ];
+
   return (
-    <div>
+    <div className="post_wrapper">
       {/* Header */}
       <div className="post_header">
         <img src={profilePic} alt="profile" />
@@ -13,11 +25,48 @@ export default function Header({ username, profilePic, postPhoto, caption }) {
       <div className="post_photo">
         <img src={postPhoto} alt="" />
       </div>
+      <div>
+        {/* Buttons */}
+        <div className="post_footer">
+          <div className="post_buttons">
+            <div className="like_comm_share">
+              <img src={Like} alt="" />
 
-      {/* Buttons */}
-      {/* Caption */}
-      {/* Comments */}
-      {/* Input */}
+              <img src={Comment} alt="" />
+
+              <img src={Share} alt="" />
+            </div>
+            <div className="post_bookmark">
+              <img src={Bookmark} alt="" />
+            </div>
+          </div>
+          <p>20,500 likes</p>
+        </div>
+        {/* Caption */}
+        <div className="post_caption">
+          <p>{username}</p>
+          <p>{caption}</p>
+          {/* View all comments */}
+          <p>View all 250 comments</p>
+        </div>
+
+        {/* Comments */}
+        <div>
+          {comments.map((comment) => (
+            <div>
+              <div>
+                <p>{comment.username}</p>
+                <p>{comment.comment}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* Input */}
+        <div className="post_comments">
+          <input type="text" placeholder="Add a comment" />
+          <button>Post</button>
+        </div>
+      </div>
     </div>
   );
 }
