ac4c7928749e40f817ba365ed45b71ba056e586d
iachiru <bistancu@gmail.com>
2022-12-03 17:26:15 +0000

    fix: stories bug fixed, no errors in the console now

--- real-u-gram/src/storiesRelated/Story.js
+++ real-u-gram/src/storiesRelated/Story.js
@@ -2,11 +2,9 @@ import React from "react";
 
 export default function Story({ username, avatar }) {
   return (
-    <>
-      <div className="storyDiv" key={username}>
-        <img className="storyPic" src={avatar} alt="story pic" />
-        <p className="storyName">{username}</p>
-      </div>
-    </>
+    <div className="storyDiv" key={username}>
+      <img className="storyPic" src={avatar} alt="story pic" />
+      <div className="storyName">{username}</div>
+    </div>
   );
 }
